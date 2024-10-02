bf3d6e778e59d676d382655af53cc7d3ccb74223
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-10-02 14:54:02 +0530

    Created pets model

---
+++ lib/models/pets_model.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class PetsModel {
+  String name;
+  String breed;
+  String image;
+  String age;
+  String sex;
+  String origin;
+  Color color;
+  String description;
+
+  PetsModel({
+    required this.image,
+    required this.name,
+    required this.breed,
+    required this.sex,
+    required this.age,
+    required this.origin,
+    required this.color,
+    required this.description,
+  });
+}
